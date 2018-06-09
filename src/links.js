5135ada8ef1bf0f8d9a21a2742d338f444dc1e25
Mark Kvetny <m.kvetny@outlook.com>
2018-06-09 18:25:20 +0200

    added some sample data

--- src/links.js
+++ src/links.js
@@ -1,4 +1,5 @@
 export default [
-  {name: 'the cruise', special: 'doku-special', url: 'https://www.youtube.com/watch?v=9awJCyjt550'},
-  {name: 'kind of a funny story', url: 'https://www.youtube.com/watch?v=cyFCpQK-j7U'}
+  { name: 'the cruise', special: 'Doku-Special', url: 'https://www.youtube.com/watch?v=9awJCyjt550' },
+  { name: 'kind of a funny story', special: 'Küken-Kino!', url: 'https://www.youtube.com/watch?v=cyFCpQK-j7U' },
+  { name: 'waltz with bashir', url: 'https://www.youtube.com/watch?v=ylzO9vbEpPg' }
 ]
