2976f43bd4f0c13d275337c0d136f76d395d6b56
Mark Kvetny <m.kvetny@outlook.com>
2018-06-09 18:03:39 +0200

    improved responsiveness

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,10 @@ import React from 'react'
 
 export default () => (
   <header>
-    <h1>Filmrausch Trailerabend</h1>
+    <span role="img" aria-label="Movie Camera" className="header__emoji">🎥</span>
+    <h1>
+      Filmrausch Trailerabend
+    </h1>
+    <span role="img" aria-label="Clapper Board" className="header__emoji">🎬</span>
   </header>
 )
